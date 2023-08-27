00058aeadb07d8b7fed7bfe4c5d83bdbc5e3e06c
v-arundhathi <vb.arundhathi@gmail.com>
2023-08-27 19:25:45 +0530

    created

---
+++ EXPERMIENT 17.R
@@ -0,0 +1,18 @@
+# Sample vector of values
+values <- c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12)
+
+# Sample vector of dimensions
+dims <- c(3, 2, 2)  # Creating a 3x2x2 array
+
+# Naming the dimensions
+dim_names <- list(
+  c("Dim1_1", "Dim1_2", "Dim1_3"),
+  c("Dim2_1", "Dim2_2"),
+  c("Dim3_1", "Dim3_2")
+)
+
+# Create the array
+arr <- array(values, dim = dims, dimnames = dim_names)
+
+# Print the array
+print(arr)
