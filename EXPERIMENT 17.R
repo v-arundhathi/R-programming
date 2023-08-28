afdbf21fac5d594303d4d35081e90a5af8068606
v-arundhathi <114045596+v-arundhathi@users.noreply.github.com>
2023-08-28 10:04:32 +0530

    Update EXPERIMENT 17.R

--- EXPERIMENT 17.R
+++ EXPERIMENT 17.R
@@ -1,14 +1,14 @@
+# Input vector of values
 values <- c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12)
 
-dims <- c(3, 2, 2)  # Creating a 3x2x2 array
+# Input vector of dimensions
+dims <- c(3, 2, 2)  # 3 rows, 2 columns, 2 tables
 
-# Naming the dimensions
-dim_names <- list(
-  c("Dim1_1", "Dim1_2", "Dim1_3"),
-  c("Dim2_1", "Dim2_2"),
-  c("Dim3_1", "Dim3_2")
-)
+# Names for each dimension
+dim_names <- c("Rows", "Columns", "Tables")
 
-arr <- array(values, dim = dims, dimnames = dim_names)
+# Create an array with dimensions and names
+array_with_names <- array(values, dim = dims, dimnames = list(NULL, NULL, dim_names))
 
-print(arr)
+# Print the array with dimension names
+print(array_with_names)
