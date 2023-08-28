1e0d05869dc5aadea79693c55690df7d8ed19137
v-arundhathi <114045596+v-arundhathi@users.noreply.github.com>
2023-08-28 10:06:21 +0530

    Update EXPERIMENT 17.R

--- EXPERIMENT 17.R
+++ EXPERIMENT 17.R
@@ -7,8 +7,8 @@ dims <- c(3, 2, 2)  # 3 rows, 2 columns, 2 tables
 # Names for each dimension
 dim_names <- c("Rows", "Columns", "Tables")
 
-# Create an array with dimensions and names
+# Create an array with specified dimensions and names
 array_with_names <- array(values, dim = dims, dimnames = list(NULL, NULL, dim_names))
 
-# Print the array with dimension names
+# Print the array
 print(array_with_names)
