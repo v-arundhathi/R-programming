b8d9e220bf3583c886062e9acd9306fcb51876a1
v-arundhathi <114045596+v-arundhathi@users.noreply.github.com>
2023-08-29 09:03:04 +0530

    Create EXPERIMENT 25.R

---
+++ EXPERIMENT 25.R
@@ -0,0 +1,34 @@
+exam_data <- data.frame(
+  name = c('Anastasia', 'Dima', 'Katherine', 'James', 'Emily', 'Michael', 'Matthew', 'Laura', 'Kevin', 'Jonas'),
+  score = c(12.5, 9, 16.5, 12, 9, 20, 14.5, 13.5, 8, 19),
+  attempts = c(1, 3, 2, 3, 2, 3, 1, 1, 2, 1),
+  qualify = c('yes', 'no', 'yes', 'no', 'no', 'yes', 'yes', 'no', 'no', 'yes')
+)
+print(exam_data)
+selected_data <- exam_data[c(3, 5), c(1, 3)]
+
+print(selected_data)
+Country <- c("USA", "USA", "USA", "USA", "UK", "USA", "USA", "India", "USA", "USA")
+exam_data$country <- Country
+
+print(exam_data)
+new_exam_data <- data.frame(
+  name = c('Robert', 'Sophia'),
+  score = c(10.5, 9),
+  attempts = c(1, 3),
+  qualify = c('yes', 'no'),
+  country = c('USA', 'USA')
+)
+combined_data <- rbind(exam_data, new_exam_data)
+
+print(combined_data)
+exam_data <- data.frame(
+  name = c('Anastasia', 'Dima', 'Katherine', 'James', 'Emily', 'Michael', 'Matthew', 'Laura', 'Kevin', 'Jonas'),
+  score = c(12.5, 9, 16.5, 12, 9, 20, 14.5, 13.5, 8, 19),
+  attempts = c(1, 3, 2, 3, 2, 3, 1, 1, 2, 1),
+  qualify = c('yes', 'no', 'yes', 'no', 'no', 'yes', 'yes', 'no', 'no', 'yes')
+)
+sorted_exam_data <- exam_data[order(exam_data$name, exam_data$score), ]
+print(sorted_exam_data)
+data<-read.csv("/Users/arundhathi/Documents/exam_data.csv")
+print(data)
