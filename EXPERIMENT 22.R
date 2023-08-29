38770a51cda0ee8428057ca0bb8c70bd8c82f039
v-arundhathi <vb.arundhathi@gmail.com>
2023-08-29 13:46:30 +0530

    create

--- EXPERIMENT 22.R
+++ EXPERIMENT 22.R
@@ -1,9 +1,6 @@
 array1 <- matrix(1:9, nrow = 3)
 array2 <- matrix(10:18, nrow = 3)
 array3 <- matrix(19:27, nrow = 3)
-print(array1)
-print(array2)
-print(array3)
 
 # Combine the arrays as specified
 combined_array <- rbind(array1[1, ], array2[1, ], array3[1, ])
