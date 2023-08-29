3757a744691718495c4377e294d3ae9c6aad7232
v-arundhathi <114045596+v-arundhathi@users.noreply.github.com>
2023-08-29 09:20:08 +0530

    Update EXPERIMENT 27.R

--- EXPERIMENT 27.R
+++ EXPERIMENT 27.R
@@ -1,8 +1,11 @@
 # Load the dataset
 data("women")
 
+# Define the height ranges and labels
+height_ranges <- cut(women$height, breaks = c(50, 60, 70, 80), labels = c("Short", "Average", "Tall"))
+
 # Create a factor corresponding to height
-height_factor <- factor(women$height, labels = c("Short", "Average", "Tall"))
+height_factor <- factor(height_ranges)
 
 # Print the first few rows of the factor
 print(height_factor)
